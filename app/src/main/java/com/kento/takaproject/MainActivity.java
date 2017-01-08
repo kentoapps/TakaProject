813da276c91118bb72f185d672020113d4fa2788
Takaharu Sakamoto <makabu.sakamoto@gmail.com>
2017-01-08 11:34:21 +0900

    タスク1完了

--- app/src/main/java/com/kento/takaproject/MainActivity.java
+++ app/src/main/java/com/kento/takaproject/MainActivity.java
@@ -1,7 +1,9 @@
 package com.kento.takaproject;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void btnSend_onClick(View v) {
+        Intent i = new Intent(this, com.kento.takaproject.DetailActivity.class);
+        startActivity(i);
     }
 }
